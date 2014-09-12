05f6ae06a294151fb410d857ed3b30ad40e03a6e
benjamin soulas <benjamin.soulas45@gmail.com>
2014-09-12 11:20:20 +0200

    tp maven intial (Mise A Niveau)

---
+++ tpmaven/src/main/java/fr/istic/master2/App.java
@@ -0,0 +1,16 @@
+package fr.istic.master2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    
+    public void uneFonction(){
+    	System.out.println();
+    }
+}
